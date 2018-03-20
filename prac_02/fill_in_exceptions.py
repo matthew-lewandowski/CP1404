29c8c23491904d6e185c16f2595d3dab90c33a8e
matthew-lewandowski <37138972+matthew-lewandowski@users.noreply.github.com>
2018-03-20 23:42:25 +1000

    had to delete and re commit everything

--- prac_02/fill_in_exceptions.py
+++ prac_02/fill_in_exceptions.py
@@ -9,6 +9,6 @@ while not finished:
     try:
         result = int(input("Enter a number: "))
         finished = True
-    except ValueError:  # TODO - add something after except
+    except ValueError:
         print("Please enter a valid integer.")
 print("Valid result is:", result)
