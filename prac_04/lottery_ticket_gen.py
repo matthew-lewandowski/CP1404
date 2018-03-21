5167fc18ab733f64e952412d723048f37c0973b4
matthew-lewandowski <37138972+matthew-lewandowski@users.noreply.github.com>
2018-03-21 12:25:37 +1000

    added quick pick lottery generator

--- prac_04/lottery_ticket_gen.py
+++ prac_04/lottery_ticket_gen.py
@@ -1,7 +1,23 @@
+import random
+
+
 def main():
-    
+    quick_pick = int(input("Enter amount of quick picks: "))
+    for picks in range(quick_pick):
+        numbers = generates_picks()
+        print(" ".join(numbers))
 
 
+def generates_picks():
+    numbers = []
+    while len(numbers) < 5:
+        random_number = random.randint(0, 46)
+        if random_number in numbers:
+            continue
+        numbers.append(random_number)
+    numbers.sort()
+    fake_numbers = [str(number) for number in numbers]
+    return fake_numbers
 
 
 main()
