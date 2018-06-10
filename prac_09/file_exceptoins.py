d0071d6c7a53b77cd82893e7896cbe5db9a374d0
matthew-lewandowski <37138972+matthew-lewandowski@users.noreply.github.com>
2018-06-10 14:58:41 +1000

    everything before test

--- prac_09/file_exceptoins.py
+++ prac_09/file_exceptoins.py
@@ -5,14 +5,16 @@ def main():
 
 def get_longest_line(file_name):
     file_object = open(file_name, mode="r")
-    longest_line = 0
-    max_length = 0
-    for i, line in enumerate(file_object):
-        if len(line) > max_length:
-            max_length = len(line)
-            longest_line = i
+    # longest_line = 0
+    # max_length = 0
+    # for i, line in enumerate(file_object):
+    #     if len(line) > max_length:
+    #         max_length = len(line)
+    #         longest_line = i
+    results = max([(len(line), i) for i, line in enumerate(file_object)])
     file_object.close()
-    return (longest_line + 1), max_length
+    # return (longest_line + 1), max_length
+    return results[1] + 1, results[0]
 
 
 main()
